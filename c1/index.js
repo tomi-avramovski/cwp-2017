dd4ecda27e9cd04e9dc61d20d1f1b880dc30f641
zeratul94 <tomi.avramovski@gmail.com>
2017-11-16 13:34:52 +0100

    c1

---
+++ c1/index.js
@@ -0,0 +1,17 @@
+console.log('Hello world!');
+
+var ime = "Bojan"; // string (niza od karakteri)
+var godini = 35; // number
+var visina = 182.5; // number 
+var temperatura = -22.5; // number 
+var classEnded = true; // boolean
+
+console.log(ime);
+console.log(godini);
+console.log(visina);
+console.log(temperatura);
+console.log(classEnded);
+
+godinaNaRagjanje = 1982; //camel case style
+godina_na_ragjanje = 1982; // snake case style
+godinanaragjanje = 1982; // BAD case!
