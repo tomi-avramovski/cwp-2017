672be7224b9a6e2d7769da4e19230e80a7f790d7
zeratul94 <tomi.avramovski@gmail.com>
2017-12-26 13:25:22 +0100

    Update main.js

--- Weather Widget/main.js
+++ Weather Widget/main.js
@@ -19,17 +19,3 @@ document.getElementById("submit").addEventListener("click", function(e){
         });
     };
   });
-// var city 
-// kreirame nova varijabla city
-// = dodeluivame vrednost (koja?)
-// se sto e desno od ednakvoto se dodeluva na city
-// sakame da zeeme tekstot vo input poleto
-// prvo treba da go trargertirame elementot
-// document (elementot e vo dokumentot ocigledno) get element by id (spored id locirame) e sea ako go ostavime vaka ke se zeme celiot html sto e so toj ID t.e. vo city ke ima <input type> celoto toa, a nasn i treba samo tekstot vo nego, a tekstot e vo subelement value, so tocka pristapuvame do toj subelement i zatoa dodavame .value
-
-// e sea otkako city go ima toa so e pisano vo input fieldot - treba da vbidime dali e prazno posto moze prazno bilo
-// za da vidime dali e prazno proveruvame dali dolzinata na tekstot e 0
-// ako e nula togas ustv nema tekst
-// ako e se osven nula (primer bukvata A or w./e) da rece eror
-// greska** ako e se ossven nula da produzi u toa so ni treba
-// ako e 0 da javi eror
